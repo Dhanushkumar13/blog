507ab4407dbf09583553e94273eeeb3f09c16816
dhanushkumar13 <dhanushpappu99@gmail.com>
2024-05-02 10:28:26 +0530

    added cors with origin

--- backend-api/index.js
+++ backend-api/index.js
@@ -18,7 +18,7 @@ require("./configs/dbConfig");
 const salt = bcrypt.genSaltSync(10);
 const secret = 'awdsadkalsdk129843knasdja'
 
-app.use(cors({credentials: true, origin:'http://localhost:3000'}));
+app.use(cors({credentials: true, origin:'https://blog-1-0bqs.onrender.com'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'))
