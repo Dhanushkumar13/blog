40bf14181021fa88e4228d9cfbe20c3df8f004fa
dhanushkumar13 <dhanushpappu99@gmail.com>
2024-05-02 10:53:40 +0530

    changed cors origin

--- backend-api/index.js
+++ backend-api/index.js
@@ -18,11 +18,7 @@ require("./configs/dbConfig");
 const salt = bcrypt.genSaltSync(10);
 const secret = 'awdsadkalsdk129843knasdja'
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://blog-indol-one-10.vercel.app');
-    // Other CORS headers if needed
-    next();
-});
+app.use(cors({credentials: true, origin:'https://blog-indol-one-10.vercel.app/'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'))
