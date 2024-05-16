6cbe0c087b65679b06f65355eb1eefac2d2b9cf5
dhanushkumar13 <dhanushpappu99@gmail.com>
2024-05-16 11:54:19 +0530

    post page modificaiton

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -6,7 +6,7 @@ export default function PostPage(){
     const [postInfo, setPostInfo] = useState(null)
     const {id} = useParams();
     useEffect(() => {
-        fetch(`https://blog-1-0bqs.onrender.com/${id}`)
+        fetch(`https://blog-1-0bqs.onrender.com/post/${id}`)
         .then(response => response.json())
         .then(postInfo => {
             setPostInfo(postInfo);
